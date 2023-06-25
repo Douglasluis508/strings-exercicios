b198c402fc1b03c7befeefb3e57e3d9e52ab84ae
Douglasluis508 <douglasluis508@gmail.com>
2023-06-25 12:50:21 -0300

    Exercícios de strings

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,3 +1,31 @@
-function imprimirComidasFavoritas (){
 
+// # Exercício 1
+// **Atenção:** para esse exercício você deve adicionar todas as soluções dentro da função 
+// **imprimirComidasFavoritas**.
+
+// Faça um programa que pergunte ao usuário seu **nome** e suas **três comidas favoritas**. 
+// Em seguida, imprima no console a seguinte mensagem:
+    
+   
+//     Estas são as comidas favoritas de nomeDoUsuario:
+//     - Comida1
+//     - Comida2
+//     - Comida3
+  
+    
+// Você deve usar apenas um `console.log()` para isso
+
+
+
+const imprimirComidasFavoritas = ()=>{
+    const nome = prompt('Qual seu nome?');
+    const comida1 = prompt('Qual sua comida favorita?');
+    const comida2 = prompt('Qual sua comida favorita?');
+    const comida3 = prompt('Qual sua comida favorita?');
+    console.log(`Estas são as comidas favoritas de ${nome}:
+    - ${comida1}
+    - ${comida2}
+    - ${comida3}`)
+    return [nome, comida1, comida2, comida3];
 }
+imprimirComidasFavoritas()
